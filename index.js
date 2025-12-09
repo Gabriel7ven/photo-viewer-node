c6b82a0d0453b890a5592713460e221c771fef58
Gabriel7ven <gabriel.oficial@yahoo.com>
2025-12-09 17:50:48 -0300

    new excel data

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import readXlsxFile from 'read-excel-file/node'
 
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const staticPath = path.join(__dirname, 'public');
 
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, "public")));
 
@@ -21,7 +24,7 @@ app.get("/", (req, res) => {
 
 
 
-readXlsxFile('data.xlsx').then(function (data) {
+readXlsxFile('data1.xlsx').then(function (data) {
                     const headers = data[0];
                     const jsonData = [];
                     for (let i = 1; i < data.length; i++) {
@@ -41,20 +44,21 @@ readXlsxFile('data.xlsx').then(function (data) {
 
     for(let i=0; i < jsonData.length; i++) {
             app.get(`/${jsonData[i]["MSLINK"]}`, (req, res) => {
-                res.send(`  <style>
-                                .fotos {
-                                    display: flex;
-                                    gap: 20px;
-                                    padding: 50px;
-                                }
-                                img {
-                                    width: 50%;
-                                }
-                            </style>
-                            <div class="fotos">
-                                <img src="${jsonData[i]["LINK FOTO 1"]}" alt="imagem de poste">
-                                <img src="${jsonData[i]["LINK FOTO 2"]}" alt="imagem de poste">
-                            </div>
+                res.send(`  <!DOCTYPE html>
+                            <html lang="en">
+                                <head>
+                                    <meta charset="UTF-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                    <link rel="stylesheet" href="/css/styles.css">
+                                    <title>Document</title>
+                                </head>
+                                <body>
+                                    <div class="fotos">
+                                        <img src="${jsonData[i]["LINK FOTO 1"]}" alt="imagem de poste">
+                                        <img src="${jsonData[i]["LINK FOTO 2"]}" alt="imagem de poste">
+                                    </div>
+                                </body>
+                            </html>
                             
                         `);
             })
@@ -64,9 +68,12 @@ readXlsxFile('data.xlsx').then(function (data) {
 });
 
 
-
+app.get("/data/:id",(req, res) => {
+    const id = req.params.id
+    res.send("id")
+})
 // Inicializar
 // initializeServer();
 app.listen(port, () => {
             console.log(`Servidor rodando em http://localhost:${port}`);
-        });
+});
